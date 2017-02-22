dea6f61687b871447f96357e52abef4c694c7db7
Kareem Ali <lkareem.alil@gmail.com>
2017-02-22 06:36:33 +0200

    simple tb with multiple ethernet packets

--- simple_epd_tb.v
+++ simple_epd_tb.v
@@ -21,19 +21,14 @@ wire [3:0] valid_packet_counter;
 initial begin
   $dumpfile("simple_epd_tb.vcd");
   $dumpvars(0,simple_epd_tb);
-  // clock = 0;
-  // reset = 0;
-  // control = 0;
-  // data = 0;
-  // #4 reset = 1;
-  // reset = 0;
-  // control = 1;
+
   clock = 1;
   reset = 1;
   control = 1;
-  // data = 0;
-  #4 reset = 0;
+  #5 reset = 0;
   
+
+  //first packet
   #2 data = 8'h55;#2 data = 8'h55;#2 data = 8'h55;#2 data = 8'h55; // Preamble
   #2 data = 8'h55;#2 data = 8'h55;#2 data = 8'h55;#2 data = 8'hD5;
 
@@ -56,8 +51,61 @@ initial begin
   #2 data = 8'h55;#2 data = 8'h56;#2 data = 8'h57;#2 data = 8'h58;#2 data = 8'h59;
   #2 data = 8'h55;#2 data = 8'h56;#2 data = 8'h57;#2 data = 8'h58;#2 data = 8'h59;
 
-  control = 1;
-  #2 data = 8'h00;
+  #4 reset = 1;
+  #4 reset = 0;
+  #2 control = 0;
+  data = 8'h00;
+  #2 control = 1;
+
+  //second packet
+  data = 8'h55;#2 data = 8'h55;#2 data = 8'h55;#2 data = 8'h55; // Preamble
+  #2 data = 8'h55;#2 data = 8'h55;#2 data = 8'h55;#2 data = 8'hD5;
+
+  #2 data = 8'h01;#2 data = 8'h02;#2 data = 8'h03;#2 data = 8'h04; // DST
+  #2 data = 8'h05;#2 data = 8'h06;
+
+  #2 data = 8'hFF;#2 data = 8'hFE;#2 data = 8'hFD;#2 data = 8'hFC; // SRC
+  #2 data = 8'hFB;#2 data = 8'hFA;
+
+  #2 data = 8'h08;#2 data = 8'h00; // Type_Length
+
+  #2 data = 8'h55;#2 data = 8'h56;#2 data = 8'h57;#2 data = 8'h58;#2 data = 8'h59; // payload
+  #2 data = 8'h55;#2 data = 8'h56;#2 data = 8'h57;#2 data = 8'h58;#2 data = 8'h59;
+  #2 data = 8'h55;#2 data = 8'h56;#2 data = 8'h57;#2 data = 8'h58;#2 data = 8'h59;
+  #2 data = 8'h55;#2 data = 8'h56;#2 data = 8'h57;#2 data = 8'h58;#2 data = 8'h59;
+  #2 data = 8'h55;#2 data = 8'h56;#2 data = 8'h57;#2 data = 8'h58;#2 data = 8'h59;
+  #2 data = 8'h55;#2 data = 8'h56;#2 data = 8'h57;#2 data = 8'h58;#2 data = 8'h59;
+  #2 data = 8'h55;#2 data = 8'h56;#2 data = 8'h57;#2 data = 8'h58;#2 data = 8'h59;
+  #2 data = 8'h55;#2 data = 8'h56;#2 data = 8'h57;#2 data = 8'h58;#2 data = 8'h59;
+  #2 data = 8'h55;#2 data = 8'h56;#2 data = 8'h57;#2 data = 8'h58;#2 data = 8'h59;
+  #2 data = 8'h55;#2 data = 8'h56;#2 data = 8'h57;#2 data = 8'h58;#2 data = 8'h59;
+
+  #2 control = 0;
+  data = 8'h00;#2 data = 8'h00;#2 data = 8'h00;#2 data = 8'h00;//multiple IFGs
+  #2 control = 1;
+
+  //third packet
+  data = 8'h55;#2 data = 8'h55;#2 data = 8'h55;#2 data = 8'h55; // Preamble
+  #2 data = 8'h55;#2 data = 8'h55;#2 data = 8'h55;#2 data = 8'hD5;
+
+  #2 data = 8'h01;#2 data = 8'h02;#2 data = 8'h03;#2 data = 8'h04; // DST
+  #2 data = 8'h05;#2 data = 8'h06;
+
+  #2 data = 8'hFF;#2 data = 8'hFE;#2 data = 8'hFD;#2 data = 8'hFC; // SRC
+  #2 data = 8'hFB;#2 data = 8'hFA;
+
+  #2 data = 8'h08;#2 data = 8'h00; // Type_Length
+
+  #2 data = 8'h55;#2 data = 8'h56;#2 data = 8'h57;#2 data = 8'h58;#2 data = 8'h59; // payload
+  #2 data = 8'h55;#2 data = 8'h56;#2 data = 8'h57;#2 data = 8'h58;#2 data = 8'h59;
+  #2 data = 8'h55;#2 data = 8'h56;#2 data = 8'h57;#2 data = 8'h58;#2 data = 8'h59;
+  #2 data = 8'h55;#2 data = 8'h56;#2 data = 8'h57;#2 data = 8'h58;#2 data = 8'h59;
+  #2 data = 8'h55;#2 data = 8'h56;#2 data = 8'h57;#2 data = 8'h58;#2 data = 8'h59;
+  #2 data = 8'h55;#2 data = 8'h56;#2 data = 8'h57;#2 data = 8'h58;#2 data = 8'h59;
+  #2 data = 8'h55;#2 data = 8'h56;#2 data = 8'h57;#2 data = 8'h58;#2 data = 8'h59;
+  #2 data = 8'h55;#2 data = 8'h56;#2 data = 8'h57;#2 data = 8'h58;#2 data = 8'h59;
+  #2 data = 8'h55;#2 data = 8'h56;#2 data = 8'h57;#2 data = 8'h58;#2 data = 8'h59;
+  #2 data = 8'h55;#2 data = 8'h56;#2 data = 8'h57;#2 data = 8'h58;#2 data = 8'h59;
 
   #10 $finish;
 end
