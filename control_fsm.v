8a931c53b5609140e96ad62a8b1268a17c428db7
Kareem Ali <lkareem.alil@gmail.com>
2017-03-01 00:34:54 +0200

    adding all tests where ifg and cntrl during payload fails else succeed

--- control_fsm.v
+++ control_fsm.v
@@ -30,7 +30,7 @@ reg [1:0] state = STATE0;
 reg [1:0] next_state = STATE0;
 
 assign enable_header = control && !state;
-assign enable_payload = control && state;
+assign enable_payload = control & state;
 
 // always @(*) begin
 // 	enable_header <= control && !state;
@@ -51,9 +51,9 @@ always @(type_length_valid or packet_size_valid) begin
 			if(packet_size_valid == 1'b1)begin
 				next_state = STATE2;
 			end
-			if(control == 1'b0)begin
-				next_state = STATE0;
-			end
+			// if(control == 1'b0)begin
+			// 	next_state = STATE0;
+			// end
 		end
 		STATE2:begin
 			next_state = STATE2;
@@ -65,42 +65,18 @@ always @(type_length_valid or packet_size_valid) begin
 end
 
 always @(posedge clock) begin
-	// $display("%b",state);
-	// // $display("%b",state != 2'bxx);
-	// $display("%b",state != STATE0 || state !=STATE1 || state !=STATE2);
-	// $display("--------------------------");
-	// if(state != STATE0 || state !=STATE1 || state !=STATE2)begin
-	// 	$display("a7eh");
+	// reset and control are synchronus active high
+	// if (reset == 1'b1 || control == 1'b0) begin
+	// 	state <= STATE0;
 	// end
 	if (reset == 1'b1) begin // reset is synchronus active high
 		state <= STATE0;
 		// enable_header = 1'b1;
 		// enable_payload = 1'b0;
 	end
-	// else if(state != STATE0 || state !=STATE1 || state !=STATE2)begin
-	// 	state = STATE0;
-	// end
 	else begin
 		state = next_state; //blocking assignment
-		// case(state)
-		// 	STATE0:begin
-		// 		enable_header <= 1'b1;
-		// 		enable_payload <= 1'b0;
-		// 	end
-		// 	STATE1:begin
-		// 		enable_header <= 1'b0;
-		// 		enable_payload <= 1'b1;
-		// 	end
-		// 	STATE2:begin
-		// 		enable_header <= 1'b0;
-		// 		enable_payload	 <= 1'b0;
-		// 	end
-		// 	default: state = STATE0;
-		// endcase
 	end
-	// $display(state);
-	// $display(state == 2'bxx);
-	
 end
 
 endmodule
---
+++ tests/multiple_mixed_packets_rst.v
@@ -0,0 +1,145 @@
+// Test triggers reset between multiple valid and invalid packets
+`include "epd.v"
+
+module multiple_mixed_packets_rst;
+
+reg clock, reset, control;
+reg [7:0] data;
+
+wire preamble_valid, dst_addr_valid, src_addr_valid,
+	type_length_valid, packet_size_valid;
+wire [3:0] valid_packet_counter;
+
+epd U_epd(
+clock,
+reset,
+data,
+control,
+preamble_valid,
+dst_addr_valid,
+src_addr_valid,
+type_length_valid,
+packet_size_valid,
+valid_packet_counter 
+);
+
+
+initial begin
+	$dumpfile("multiple_mixed_packets_rst.vcd");
+	$dumpvars(0,multiple_mixed_packets_rst);
+	
+	clock = 0;
+	
+	control = 1;
+	data = 0;
+	reset = 0;
+	// #4 reset = 0;
+	
+	//***************************** First packet (VALID) ******************************
+	//valid preamble
+	repeat (7) #2 data = 8'h55;
+	#2 data = 8'hD5;
+
+	//valid DST
+	#2 data = 8'h01; #2 data = 8'h02; #2 data = 8'h03; #2 data = 8'h04;
+    #2 data = 8'h05; #2 data = 8'h06;
+
+    //valid SRC
+    #2 data = 8'hFF; #2 data = 8'hFE; #2 data = 8'hFD; #2 data = 8'hFC;
+    #2 data = 8'hFB; #2 data = 8'hFA;
+
+	//valid type_length
+	#2 data = 8'h08; #2 data = 8'h00;
+
+	//send payload and crc
+	repeat (49) #2 data = 8'h55;#2 data = 8'hFF;
+
+	//send one IFG
+	#2 control = 0;
+    data = 8'h00;
+    #2 control = 1;
+
+    //***************************** Second packet (INVALID) ******************************
+	//valid preamble
+	repeat (7) #2 data = 8'h55;
+	#2 data = 8'hD5;
+
+	//valid DST
+	#2 data = 8'h01; #2 data = 8'h02; #2 data = 8'h03; #2 data = 8'h04;
+    #2 data = 8'hFF; #2 data = 8'hFF; //INVALID DESTINATION ADDRESS
+
+    //valid SRC
+    #2 data = 8'hFF; #2 data = 8'hFE; #2 data = 8'hFD; #2 data = 8'hFC;
+    #2 data = 8'hFB; #2 data = 8'hFA;
+
+	//valid type_length
+	#2 data = 8'h08; #2 data = 8'h00;
+
+	//send payload and crc
+	repeat (49) #2 data = 8'h55;#2 data = 8'h56;
+
+	//send multiple IFGs
+	#2 control = 0;
+    data = 8'h00;
+    #6 control = 1;
+
+    reset = 1; //TRIGGERS RESET
+    #4 reset = 0;
+
+    //***************************** Third packet (INVALID) ******************************
+	//valid preamble
+	repeat (7) #2 data = 8'h55;
+	#2 data = 8'hD5;
+
+	//valid DST
+	#2 data = 8'h01; #2 data = 8'h02; #2 data = 8'h03; #2 data = 8'h04;
+    #2 data = 8'h05; #2 data = 8'h06; 
+
+    //valid SRC
+    #2 data = 8'hFF; #2 data = 8'hFE; #2 data = 8'hFD; #2 data = 8'hFC;
+    #2 data = 8'h00; #2 data = 8'h00; //INVALID SOURCE ADDRESS
+
+	//valid type_length
+	#2 data = 8'h08; #2 data = 8'h00;
+
+	//send payload and crc
+	repeat (49) #2 data = 8'h55;#2 data = 8'h56;
+
+	//send multiple IFGs
+	#2 control = 0;
+    data = 8'h00;
+    #6 control = 1;
+
+    //***************************** Fourth packet (VALID) ******************************
+	//valid preamble
+	repeat (7) #2 data = 8'h55;
+	#2 data = 8'hD5;
+
+	//valid DST
+	#2 data = 8'h01; #2 data = 8'h02; #2 data = 8'h03; #2 data = 8'h04;
+    #2 data = 8'h05; #2 data = 8'h06;
+
+    //valid SRC
+    #2 data = 8'hFF; #2 data = 8'hFE; #2 data = 8'hFD; #2 data = 8'hFC;
+    #2 data = 8'hFB; #2 data = 8'hFA;
+
+	//valid type_length
+	#2 data = 8'h08; #2 data = 8'h00;
+
+	//send payload and crc
+	repeat (49) #2 data = 8'h55;#2 data = 8'h56;
+
+	//send one IFG
+	#2 control = 0;
+    data = 8'h00;
+    #2 control = 1;
+
+    #10 $finish;
+end
+
+
+//clock generator
+always
+	#1 clock =~ clock;
+
+endmodule
